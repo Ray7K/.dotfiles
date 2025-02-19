77c0517dda12d10ab509300ecf5cd7053640d1c1
Ray7K <173786719+Ray7K@users.noreply.github.com>
2025-02-19 13:56:39 +0530

    fix(nvim): lualine config fix

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,25 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = {
-    options = {
-      theme = "auto",
-      globalstatus = vim.o.laststatus == 3,
-      disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
-      component_separators = { "" },
-      -- section_separators = { "" },
-    },
-  },
+  opts = function()
+    local auto = require("lualine.themes.auto")
+    local lualine_modes = { "insert", "normal", "visual", "command", "replace", "inactive", "terminal" }
+
+    -- Modify the background of the `c` section for each mode
+    for _, field in ipairs(lualine_modes) do
+      if auto[field] and auto[field].c then
+        auto[field].c.bg = "NONE"
+      end
+    end
+
+    -- Return modified options
+    return {
+      options = {
+        theme = auto, -- Apply the modified theme
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+        component_separators = { "" },
+        section_separators = { "" },
+      },
+    }
+  end,
 }
