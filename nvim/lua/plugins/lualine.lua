15ff0c47c44790c86a484f457d8007c5e7b9a816
Ray7K <173786719+Ray7K@users.noreply.github.com>
2025-02-19 13:38:31 +0530

    fix(nvim): statusline ui fixes

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
       globalstatus = vim.o.laststatus == 3,
       disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
       component_separators = { "" },
-      section_separators = { "" },
+      -- section_separators = { "" },
     },
   },
 }
