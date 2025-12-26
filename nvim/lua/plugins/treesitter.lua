a2ce6a978f2d1f39c36d33a47c8a99284bc80f39
Ray7K <173786719+Ray7K@users.noreply.github.com>
2025-12-26 14:30:54 +0530

    nvim: Update treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,126 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        config = function()
+          local ts_repeat = require 'nvim-treesitter-textobjects.repeatable_move'
+          local move = require 'nvim-treesitter-textobjects.move'
+          local select = require 'nvim-treesitter-textobjects.select'
+
+          require('nvim-treesitter-textobjects').setup {
+            move = { set_jumps = true },
+            select = {
+              lookahead = true,
+              selection_modes = {
+                ['@function.outer'] = 'V',
+                ['@class.outer'] = 'V',
+              },
+            },
+          }
+
+          vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat.repeat_last_move_next)
+          vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat.repeat_last_move_previous)
+
+          vim.keymap.set({ 'n', 'x', 'o' }, ']f', function()
+            move.goto_next_start('@function.outer', 'textobjects')
+          end)
+          vim.keymap.set({ 'n', 'x', 'o' }, '[f', function()
+            move.goto_previous_start('@function.outer', 'textobjects')
+          end)
+
+          vim.keymap.set({ 'n', 'x', 'o' }, ']c', function()
+            move.goto_next_start('@class.outer', 'textobjects')
+          end)
+
+          vim.keymap.set({ 'n', 'x', 'o' }, '[c', function()
+            move.goto_previous_start('@class.outer', 'textobjects')
+          end)
+
+          vim.keymap.set({ 'x', 'o' }, 'af', function()
+            select.select_textobject('@function.outer', 'textobjects')
+          end)
+          vim.keymap.set({ 'x', 'o' }, 'if', function()
+            select.select_textobject('@function.inner', 'textobjects')
+          end)
+
+          vim.keymap.set({ 'x', 'o' }, 'ac', function()
+            select.select_textobject('@class.outer', 'textobjects')
+          end)
+          vim.keymap.set({ 'x', 'o' }, 'ic', function()
+            select.select_textobject('@class.inner', 'textobjects')
+          end)
+        end,
       },
-      indent = { enable = true, disable = { 'ruby' } },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    config = function()
+      local ts = require 'nvim-treesitter'
+
+      ts.install {
+        'asm',
+        'bash',
+        'bibtex',
+        'c',
+        'cpp',
+        'cmake',
+        'c-sharp',
+        'css',
+        'csv',
+        'diff',
+        'disassembly',
+        'go',
+        'html',
+        'http',
+        'java',
+        'javascript',
+        'json',
+        'kconfig',
+        'llvm',
+        'lua',
+        'luadoc',
+        'make',
+        'markdown',
+        'markdown_inline',
+        'nix',
+        'python',
+        'query',
+        'regex',
+        'rust',
+        'sql',
+        'toml',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'xml',
+        'yaml',
+        'zig',
+      }
+
+      local group = vim.api.nvim_create_augroup('TreesitterMain', { clear = true })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        callback = function(args)
+          if vim.bo[args.buf].buftype ~= '' then
+            return
+          end
+
+          pcall(vim.treesitter.start, args.buf)
+
+          vim.wo.foldmethod = 'expr'
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+          if vim.bo[args.buf].filetype ~= 'ruby' then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
   },
 }
--- vim: ts=2 sts=2 sw=2 et
